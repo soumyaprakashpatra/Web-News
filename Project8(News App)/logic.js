d5ef37f780410bf90fe14a1e29692756004ffc8b
soumyaprakashpatra <108807769+soumyaprakashpatra@users.noreply.github.com>
2023-12-28 08:52:00 +0530

    Add files via upload

---
+++ Project8(News App)/logic.js
@@ -0,0 +1,26 @@
+
+console.log("Building a News App.");
+
+// newsAccordion=document.getElementById("newsAccordion");
+
+// let news=`
+
+// <div class="accordian" id="newsAccordion" >
+//         <p>
+//             <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+//                 data-bs-target="#multiCollapseExample1" aria-expanded="false"
+//                 aria-controls="multiCollapseExample2">Display First News</button>
+
+//         </p>
+//         <div class="row">
+//             <div class="col">
+//                 <div class="collapse multi-collapse" id="multiCollapseExample1">
+//                     <div class="card card-body">
+//                         Some placeholder content for the first collapse component of this multi-collapse example. This
+//                         panel
+//                         is hidden by default but revealed when the user activates the relevant trigger.
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div> ` ;
